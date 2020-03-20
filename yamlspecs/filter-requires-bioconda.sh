a603bd6446d1fe38f15a7582ca8c92eda1191f67
nadyawilliams <nadya.williams@gmail.com>
2020-03-19 17:37:49 -0700

    update conda to 4.8.3. See notes in bioconda.yaml

--- yamlspecs/filter-requires-bioconda.sh
+++ yamlspecs/filter-requires-bioconda.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 #
-# remove erroneous requirements
-# with hard-coded paths.
+# remove erroneous requirements with hard-coded paths.
+# yes, each new conda'update' adds a few more lines.
+
 
 /usr/lib/rpm/find-requires $* | sed  \
     -e '/\/opt\/anaconda1anaconda2anaconda3/d' \
+    -e '/\/home\/conda/d' \
     -e '/\/usr\/local\/bin\/python/d' \
     -e '/libcblas.so/d' \
     -e '/import/d' \
+    -e '/^"/d' \
     -e '/^#/d'
-
-#   -e '/libcom_err.so.3/d' \
-#   -e '/libkdb5.so.9/d' \
-#   -e '/libverto.so.0/d' \
-#   -e '/from/d' \
-#   -e '/import/d' \
-#   -e '/def/d' \
-#   -e '/^#/d'
